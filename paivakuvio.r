094763e26904f2a7a268e84f2619c23eef6bb4ed
pasipasi123 <pasi.haapakorva@gmail.com>
2018-04-12 20:44:12 +0300

    Yksi rivi pois

--- paivakuvio.r
+++ paivakuvio.r
@@ -14,34 +14,45 @@ kivi <- wdata %>%
   filter(date == today() - days(1)) %>%
   mutate(osuus = round(100 - (100 * freespace / totalspace), 1))
 
-keski <- kivi %>% 
+keskiarvo <- kivi %>% 
   filter(hour(timestamp) %in% 8:20) %>% 
-  summarise(mean = round(mean(osuus, na.rm = TRUE), 2)) %>% 
-  pull(mean)
+  summarise(y = round(mean(osuus, na.rm = TRUE), 2)) %>% 
+  mutate(aika = ymd_hm(paste(today() - days(1), "05:00"))) %>% 
+  mutate(aika = if_else(y < 20, aika + hours(7), aika))
 
-keskipv <- ymd_hm(paste(today() - days(1), "05:00"))
+# keskipv <- ymd_hm(paste(today() - days(1), "05:00"))
 
 autoja <- kivi %>% filter(hour(timestamp) == 2) %>% 
   filter(row_number() == 1) %>% 
-  transmute(timestamp, osuus, autoja = totalspace - freespace) 
+  transmute(aika = timestamp, y = osuus, autoja = totalspace - freespace) 
+
+repel_pisteet <- bind_rows(keskiarvo, autoja) %>% 
+  mutate(label = c(paste0("Keskiarvo klo 8–20 ", y[1], " %"), 
+                   paste("Aamuyöllä hallissa\n", autoja[2], "autoa."))) %>% 
+  mutate(nudge_y = case_when(
+    hour(aika) > 10 ~ 30,
+         TRUE ~ 10))
 
 pv <- ggplot(kivi, aes(timestamp, osuus)) +
-  geom_hline(yintercept = keski) +
+  geom_hline(yintercept = keskiarvo$y) +
   geom_line(size = 1, color = "red") +
   labs(x = NULL,
        y = NULL,
        title = paste0("Kivisydämen asiointipaikkojen käyttöaste ", format(today() - days(1), format = "%d.%m.%Y")),
        caption = "Analyysi: Pasi Haapakorva\n@pasi_pasi, @kivisydanbot") +
   coord_cartesian(ylim = c(0, 100)) +
-  geom_text(aes(x = keskipv, y = keski + 5, label = paste0("Keskiarvo klo 8–20 ", keski, " %")), size = 3) +
+  # geom_text(aes(x = keskipv, y = keski + 5, label = paste0("Keskiarvo klo 8–20 ", keski, " %")), size = 3) +
+  ggrepel::geom_text_repel(data = repel_pisteet, aes(x = aika, y = y, label = label),
+                           direction = "y", nudge_y = repel_pisteet$nudge_y, size = 3) +
+  geom_point(data = autoja, aes(aika, y), size = 2, color = "blue") +
   scale_x_datetime(date_breaks = "1 hour", date_labels= "%H:%M") +
   scale_y_continuous(labels = function(x) paste(x, "%")) +
   theme_minimal() +
   theme(text = element_text(size = 8),
-                 axis.text.x = element_text(angle = 90, vjust = 0.5)) +
-  geom_point(data = autoja, size = 2, color = "blue") +
-  ggrepel::geom_text_repel(data = autoja, aes(label = paste("Aamuyöllä hallissa\n", autoja, "autoa.")), 
-                           direction = "y", nudge_y = 10, size = 3)
+                 axis.text.x = element_text(angle = 90, vjust = 0.5))
+  # ggrepel::geom_text_repel(data = autoja, aes(label = paste("Aamuyöllä hallissa\n", autoja, "autoa.")), 
+  #                          direction = "y", nudge_y = 10, size = 3) +
+  # geom_point(data = autoja, size = 2, color = "blue")
 
 ggsave("kuvio.png", h = 4, w = 5)
 
