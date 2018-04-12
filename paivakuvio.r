3bedaa9084199f7eb2381c9af4b130f899a69f3e
pasipasi123 <pasi.haapakorva@gmail.com>
2018-04-12 19:22:41 +0300

    update

--- paivakuvio.r
+++ paivakuvio.r
@@ -21,19 +21,28 @@ keski <- kivi %>%
 
 keskipv <- ymd_hm(paste(today() - days(1), "05:00"))
 
+autoja <- kivi %>% filter(hour(timestamp) == 2) %>% 
+  filter(row_number() == 1) %>% 
+  transmute(timestamp, osuus, autoja = totalspace - freespace) 
+
 pv <- ggplot(kivi, aes(timestamp, osuus)) +
   geom_hline(yintercept = keski) +
   geom_line(size = 1, color = "red") +
-  labs(x = "aika",
-       y = "käyttöaste %",
+  labs(x = NULL,
+       y = NULL,
        title = paste0("Kivisydämen asiointipaikkojen käyttöaste ", format(today() - days(1), format = "%d.%m.%Y")),
        caption = "Analyysi: Pasi Haapakorva\n@pasi_pasi, @kivisydanbot") +
   coord_cartesian(ylim = c(0, 100)) +
   geom_text(aes(x = keskipv, y = keski + 5, label = paste0("Keskiarvo klo 8–20 ", keski, " %")), size = 3) +
   scale_x_datetime(date_breaks = "1 hour", date_labels= "%H:%M") +
+  scale_y_continuous(labels = function(x) paste(x, "%")) +
+  theme_minimal() +
   theme(text = element_text(size = 8),
-                 axis.text.x = element_text(angle = 90, vjust = 0.5),
-                 axis.title.x = element_blank())
+                 axis.text.x = element_text(angle = 90, vjust = 0.5)) +
+  geom_point(data = autoja, size = 2, color = "blue") +
+  ggrepel::geom_text_repel(data = autoja, aes(label = paste("Aamuyöllä hallissa\n", autoja, "autoa.")), 
+                           direction = "y", nudge_y = 10, size = 3)
 
-ggsave("kuvio.png", height = 4, width = 5, units = "in", dpi = 300)
+ggsave("kuvio.png", h = 4, w = 5)
+
 
